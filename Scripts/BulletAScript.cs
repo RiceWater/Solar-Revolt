b35fcfccb1ee7fb884fbb9bec03d5f080446d2e5
coolm141 <115871254+coolm141@users.noreply.github.com>
2022-11-09 15:47:58 +0800

    Add files via upload

---
+++ Scripts/BulletAScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAScript : MonoBehaviour
+{
+    private GameObject target;
+    private Rigidbody2D bulletRigidBody;
+    private float moveSpeed;
+    private float rotationSpeed;
+
+    void Start()
+    {
+        bulletRigidBody = transform.gameObject.GetComponent<Rigidbody2D>();
+        moveSpeed = 20f;
+        rotationSpeed = 4f;
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+        }
+        TravelToTarget();
+        CheckOutOfBounds();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (collision.gameObject.Equals(target))
+            {
+                target.GetComponent<EnemyAttributesScript>().TakeDamage(1); //replace with tower damage (Wood's Code Needed)
+                Destroy(gameObject);
+            }
+            else
+            {
+                Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), collision.gameObject.GetComponent<Collider2D>());
+            }
+        }
+    }
+
+
+    public void SetTarget(GameObject enemy)
+    {
+        target = enemy;
+    }
+
+    private void TravelToTarget()
+    {
+        Vector3 dir = (Vector2)target.transform.position - bulletRigidBody.position;
+        dir.Normalize();
+        float rotationAmount = Vector3.Cross(dir, transform.up).z;
+        bulletRigidBody.angularVelocity = -rotationAmount * rotationSpeed;
+        bulletRigidBody.velocity = transform.up * moveSpeed;
+    }
+
+    private void CheckOutOfBounds()
+    {
+        int xbound = 50, ybound = 30;
+        if(transform.position.x > xbound || transform.position.x < -xbound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.y > ybound || transform.position.y < -ybound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Scripts/GariumScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GariumScript : MonoBehaviour
+{
+    [SerializeField] private static int garium;
+    private float second;
+
+    void Start()
+    {
+        garium = 0;
+        second = 0;
+    }
+
+    
+    void Update()
+    {
+        second += Time.deltaTime;
+        IncrementGarium();
+        ValueProofGarium();
+    }
+
+    public static int Garium
+    {
+        get { return garium; }
+        set { garium = value; }
+    }
+
+    private void IncrementGarium()
+    {
+        if (second > 0.5)
+        {
+            garium++;
+            second = 0;
+        }
+        
+    }
+
+    private void ValueProofGarium()
+    {
+        if(garium < 0)
+        {
+            garium = 0;
+        }
+    }
+}
