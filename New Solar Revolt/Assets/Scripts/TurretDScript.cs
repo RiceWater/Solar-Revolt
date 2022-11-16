c776e38c2d3e1aeaa09e3b636120a1bd3844d595
coolm141 <115871254+coolm141@users.noreply.github.com>
2022-11-16 18:57:04 +0800

    Third bullet

---
+++ New Solar Revolt/Assets/Scripts/TurretDScript.cs
@@ -0,0 +1,207 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretDScript : MonoBehaviour
+{
+    [SerializeField] private int gariumCost;
+    private Transform target;  
+
+    //For direction;
+    public Transform directionObject;
+
+    //Bullet-related
+    public float fireRate;
+    private float fireCountdown;
+
+    private bool dead = false;
+
+    //Bullet Prefabs-related
+    public GameObject pfBulletD;
+    public Transform spawnLocationBulletD;
+
+    [SerializeField] private List<GameObject> enemiesInRange = new List<GameObject>();
+
+    //targetType = G : first to goal, S : strongest, W : weakest, R : first in range
+    [SerializeField] private char targetPriority;
+
+    private void Start()
+    {
+        //InvokeRepeating("TargetEnemyNearGoal", 0f, 1f);
+        targetPriority = 'G';
+        fireRate = 0.5f;
+        fireCountdown = 0f;
+        gariumCost = 20;
+    }
+
+    private void Update()
+    {
+        SetTargetPriority();
+        if (target == null)
+        {
+            return;
+        }
+
+        RotateTowardsTarget();
+
+        //Fires the projectile
+        if(fireCountdown <= 0 && target != null)
+        {
+            FireProjectile();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    public int GariumCost
+    {
+        get { return gariumCost; }
+        set { gariumCost = value; }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemiesInRange.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemiesInRange.Remove(collision.gameObject);
+        }
+    }
+
+    private void FireProjectile()
+    {
+        GameObject bullet = Instantiate(pfBulletD, spawnLocationBulletD.position, spawnLocationBulletD.rotation);
+        bullet.GetComponent<BulletDScript>().SetTarget(target.gameObject);   
+    }
+
+    private void SetTargetPriority()
+    {
+        switch (targetPriority)
+        {
+            case 'G':
+                TargetEnemyNearGoal();
+                break;
+            case 'R':
+                TargetEnemyFirst();
+                break;
+            case 'S':
+                TargetEnemyMostHP();
+                break;
+            case 'W':
+                TargetEnemyLeastHP();
+                break;
+            default:
+                TargetEnemyNearGoal();
+                break;
+        }
+    }
+
+    //Targets the enemy closest to the endpoint 
+    private void TargetEnemyNearGoal()
+    {
+        float highestDistance = -1f;
+        if (enemiesInRange.Count == 0)
+        {
+            target = null;
+            return;
+        }
+
+        //checks if enemy is dead before targeting another enemy
+        if (target != null)
+        {
+            return;
+        }
+
+        foreach (GameObject enemy in enemiesInRange)
+        {
+            float currEnemyDistance = enemy.GetComponent<EnemyDistanceTraveledScript>().getDistanceTraveled();
+            if (currEnemyDistance > highestDistance)
+            {
+                target = enemy.transform;
+                highestDistance = currEnemyDistance;
+            }
+        }
+    }
+
+    //Targets enemy that came into range first
+    private void TargetEnemyFirst()
+    {
+        if(enemiesInRange.Count < 1)
+        {
+            return;
+        }
+
+        //checks if enemy is dead before targeting another enemy
+        if (target != null)
+        {
+            return;
+        }
+
+        target = enemiesInRange[0].transform;
+    }
+
+    //Targets the enemy with the highest health 
+    private void TargetEnemyMostHP()
+    {
+        if(enemiesInRange.Count < 1)
+        {
+            return;
+        }
+
+        //checks if enemy is dead before targeting another enemy
+        if (target != null)
+        {
+            return;
+        }
+
+        float maxHealth = -10000;
+        foreach (GameObject enemy in enemiesInRange)
+        {
+            float currEnemyHealth = enemy.GetComponent<EnemyAttributesScript>().EnemyHealth;
+            if (currEnemyHealth > maxHealth)
+            {
+                target = enemy.transform;
+                maxHealth = currEnemyHealth;
+            }
+        }
+    }
+
+    //Targets the enemy with the lowest health
+    private void TargetEnemyLeastHP()
+    {
+        if (enemiesInRange.Count < 1)
+        {
+            return;
+        }
+
+        //checks if enemy is dead before targeting another enemy
+        if (target != null)
+        {
+            return;
+        }
+
+        float minHealth = 10000;
+        foreach (GameObject enemy in enemiesInRange)
+        {
+            float currEnemyHealth = enemy.GetComponent<EnemyAttributesScript>().EnemyHealth;
+            if (currEnemyHealth < minHealth)
+            {
+                target = enemy.transform;
+                minHealth = currEnemyHealth;
+            }
+        }
+    }
+
+    private void pause()
+    {
+
+    }
+}
